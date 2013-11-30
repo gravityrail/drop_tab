e52271585640f7aa34d224488483ac485cc59517
Daniel Walmsley <goldsounds@gmail.com>
2013-11-30 09:45:34 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DropTab::Application.config.secret_token = 'dbe1aa064b251b74ef08dfd11e4c279f5696f0d9d2ec46df450f96b3c9b2bfa806191973c71b229de083a13a2e465085ae5e1d0461956df20d9f06a3886ed8b6'
